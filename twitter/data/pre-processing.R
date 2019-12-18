3c33aa7ca7fb541ba0a97c689a3fd3f7e6273d29
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2019-12-18 10:31:32 +0000

    Added pre-processing script

--- twitter/data/pre-processing.R
+++ twitter/data/pre-processing.R
@@ -2,12 +2,12 @@ library(rtweet)
 
 # tweets mentioning @TraffordCouncil or the phrase "Trafford Council"
 mentions <- search_tweets(q = '@TraffordCouncil OR "Trafford Council"', n = 18000, include_rts = FALSE, `-filter` = "replies", retryonratelimit = TRUE, lang = "en")
-write_as_csv(mentions, "data/mentions.csv")
+write_as_csv(mentions, "mentions.csv")
 
 # last 1000 tweets by @TraffordCouncil
 tweets <- get_timeline(user = "TraffordCouncil", n = 1000)
-write_as_csv(tweets, "data/tweets.csv")
+write_as_csv(tweets, "tweets.csv")
 
 # last 1000 likes by @TraffordCouncil
 likes <- get_favorites("TraffordCouncil", n = 1000)
-write_as_csv(likes, "data/likes.csv")
+write_as_csv(likes, "likes.csv")
