46b272a976e23f5d5ebaf499ccf93cf4e2130fde
James Austin <james.austin@trafford.gov.uk>
2021-05-19 19:47:56 +0100

    Added Parliament chart visualisation.

--- local_election/2021/pre-processing.R
+++ local_election/2021/pre-processing.R
@@ -1,7 +1,7 @@
 # Script for creating the datasets required for visualising the election results data etc.
 
 # Required packages
-library(tidyverse) ; library(lubridate)
+library(tidyverse) ; library(lubridate) ; library(ggpol)
 
 # Load in the full election results dataset from 2016 onwards
 election_results <- read_csv("https://www.trafforddatalab.io/open_data/elections/trafford_council_election_results.csv")
@@ -66,7 +66,9 @@ current_councillors <- elected_candidates %>%
   slice(tail(row_number(), 3))
 
 #----------------------------------
-# Amend the current_councillors dataset to produce the data for the political control visualisation
+# Political Control dataset and visualisation
+
+# Amend the current_councillors dataset to produce the data
 political_control <- current_councillors %>%
   group_by(ward) %>%
   mutate(elected_ordinal = row_number()) %>%
@@ -81,4 +83,36 @@ political_control <- current_councillors %>%
          elected_ordinal)
   
 write_csv(political_control, "data/political_control_2021.csv")
+
+# Parliament chart visualisation
+parliament_chart <- political_control %>%
+  count(councillor_party) %>%
+  mutate(colours = case_when(
+    councillor_party == "Green Party" ~ "#6ab023",
+    councillor_party == "Liberal Democrats" ~ "#fdbb30",
+    councillor_party == "Conservative Party" ~ "#0087dc",
+    TRUE ~ "#dc241f"
+  )) %>%
+  select(party = councillor_party,
+         seats = n,
+         colours) %>%
+  arrange(seats)
+
+ggplot(parliament_chart) + 
+  geom_parliament(aes(seats = seats, fill = party), colour = "#ffffff") + 
+  scale_fill_manual(values = parliament_chart$colours, labels = parliament_chart$party,
+                    guide = guide_legend(reverse = TRUE)) +
+  labs(title = "Trafford Council Seats by Party",
+       subtitle = "Following local election: 06 May 2021",
+       caption = "Source: trafford.gov.uk | @traffordDataLab",
+       fill = NULL) +
+  coord_fixed() + 
+  theme_void() +
+  theme(plot.margin = unit(rep(0.1, 4), "cm"),
+        plot.title = element_text(size = 14, face = "bold", hjust = 0.5, color = "#707070"),
+        plot.subtitle = element_text(size = 10, hjust = 0.5, color = "#212121"),
+        plot.caption = element_text(size = 7, color = "#707070", hjust = 0.96, margin = margin(t = 10, b = 5)),
+        legend.position = "bottom")
+
+ggsave("images/seats_by_party_parliament_chart_2021.png", dpi = 320, scale = 1, width = 5.35, height = 4)
 #----------------------------------
