5d18c64905e6e0339174c821f81d435f7bd3711c
James Austin <james.austin@trafford.gov.uk>
2021-06-14 18:34:06 +0100

    Added turnout data and visualisation and split the visualisation code from the pre-processing script so that this only deals with the CSV data file creation. Each visualisation now has its own script which reads in the relevant data and then creates the plot.

---
+++ local_election/2021/political_control_plot.R
@@ -0,0 +1,38 @@
+# Parliament chart visualisation showing the political control of Trafford
+
+# Required packages
+library(tidyverse) ; library(ggpol)
+
+# Load the source data
+political_control <- read_csv("data/political_control_2021.csv")
+
+parliament_chart <- political_control %>%
+  count(councillor_party) %>%
+  mutate(colours = case_when(
+    councillor_party == "Green Party" ~ "#6ab023",
+    councillor_party == "Liberal Democrats" ~ "#fdbb30",
+    councillor_party == "Conservative Party" ~ "#0087dc",
+    TRUE ~ "#dc241f"
+  )) %>%
+  select(party = councillor_party,
+         seats = n,
+         colours) %>%
+  arrange(seats)
+
+ggplot(parliament_chart) + 
+  geom_parliament(aes(seats = seats, fill = party), colour = "#ffffff") + 
+  scale_fill_manual(values = parliament_chart$colours, labels = parliament_chart$party,
+                    guide = guide_legend(reverse = TRUE)) +
+  labs(title = "Trafford Council Seats by Party",
+       subtitle = "Following local election: 06 May 2021",
+       caption = "Source: trafford.gov.uk | @traffordDataLab",
+       fill = NULL) +
+  coord_fixed() + 
+  theme_void() +
+  theme(plot.margin = unit(rep(0.1, 4), "cm"),
+        plot.title = element_text(size = 14, face = "bold", hjust = 0.5, color = "#707070"),
+        plot.subtitle = element_text(size = 10, hjust = 0.5, color = "#212121"),
+        plot.caption = element_text(size = 7, color = "#707070", hjust = 0.96, margin = margin(t = 10, b = 5)),
+        legend.position = "bottom")
+
+ggsave("images/seats_by_party_parliament_chart_2021.png", dpi = 320, scale = 1, width = 5.35, height = 4)
--- local_election/2021/pre-processing.R
+++ local_election/2021/pre-processing.R
@@ -1,7 +1,7 @@
-# Script for creating the datasets required for visualising the election results data etc.
+# Script for creating the datasets required for visualising the election results data.
 
 # Required packages
-library(tidyverse) ; library(lubridate) ; library(ggpol)
+library(tidyverse) ; library(lubridate)
 
 # Load in the full election results dataset from 2016 onwards
 election_results <- read_csv("https://www.trafforddatalab.io/open_data/elections/trafford_council_election_results.csv")
@@ -66,7 +66,7 @@ current_councillors <- elected_candidates %>%
   slice(tail(row_number(), 3))
 
 #----------------------------------
-# Political Control dataset and visualisation
+# Political Control dataset
 
 # Amend the current_councillors dataset to produce the data
 political_control <- current_councillors %>%
@@ -84,35 +84,17 @@ political_control <- current_councillors %>%
   
 write_csv(political_control, "data/political_control_2021.csv")
 
-# Parliament chart visualisation
-parliament_chart <- political_control %>%
-  count(councillor_party) %>%
-  mutate(colours = case_when(
-    councillor_party == "Green Party" ~ "#6ab023",
-    councillor_party == "Liberal Democrats" ~ "#fdbb30",
-    councillor_party == "Conservative Party" ~ "#0087dc",
-    TRUE ~ "#dc241f"
-  )) %>%
-  select(party = councillor_party,
-         seats = n,
-         colours) %>%
-  arrange(seats)
-
-ggplot(parliament_chart) + 
-  geom_parliament(aes(seats = seats, fill = party), colour = "#ffffff") + 
-  scale_fill_manual(values = parliament_chart$colours, labels = parliament_chart$party,
-                    guide = guide_legend(reverse = TRUE)) +
-  labs(title = "Trafford Council Seats by Party",
-       subtitle = "Following local election: 06 May 2021",
-       caption = "Source: trafford.gov.uk | @traffordDataLab",
-       fill = NULL) +
-  coord_fixed() + 
-  theme_void() +
-  theme(plot.margin = unit(rep(0.1, 4), "cm"),
-        plot.title = element_text(size = 14, face = "bold", hjust = 0.5, color = "#707070"),
-        plot.subtitle = element_text(size = 10, hjust = 0.5, color = "#212121"),
-        plot.caption = element_text(size = 7, color = "#707070", hjust = 0.96, margin = margin(t = 10, b = 5)),
-        legend.position = "bottom")
-
-ggsave("images/seats_by_party_parliament_chart_2021.png", dpi = 320, scale = 1, width = 5.35, height = 4)
 #----------------------------------
+# Turnout dataset for all local elections we have data for
+
+turnout <- election_results %>%
+  filter(TypeofElection == "DistrictAndBorough") %>%
+  mutate(ward_code = str_extract(ElectoralAreaURI, "E050008[0-9]{2}"),
+         election_year = year(ElectionDate)) %>%
+  select(election_year,
+         ward_code,
+         ward_name = ElectoralAreaLabel,
+         turnout = PercentageTurnout) %>%
+  distinct() # remove the multiple rows which are due to there being one row per candidate
+
+write_csv(turnout, "data/local_election_turnout.csv")
